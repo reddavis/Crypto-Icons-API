825b7134237f0cfaa752bffb9ffe9adcc9e9d48b
Red Davis <me@red.to>
2018-08-29 07:42:06 +0100

    Use redis to cache responses

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const { convert } = require('convert-svg-to-png')
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const { document } = (new JSDOM('')).window
+const fs = require('fs');
+const path = require('path');
 
 
 app.use(express.static(__dirname + '/public'));
@@ -15,48 +17,99 @@ app.get('/', function(req, res) {
 
 // GET png
 app.get('/api/:style/:currency/:size', async(req, res) => {
-  const fs = require('fs');
-  const path = require('path');
-  
   // Params
   const style = req.params.style
   const currency = req.params.currency
   const size = req.params.size
+  const cacheKey = req.path
   
+  // Redis
+  var redisRetryStrategy = function(options) {
+    if (options.error.code === 'ECONNREFUSED') 
+    {
+      return;
+    }
+  }
+  
+  const redisURL = process.env.REDIS_URL || 'http://127.0.0.1:6379'
+  var client = require('redis').createClient({
+    url : process.env.REDIS_URL,
+    return_buffers : true
+  })
+  
+  client.on('error', function (err) {
+    client.quit()
+    generatePNG(req, res, null)
+  })
+  
+  client.on('connect', function (err) {
+    // Check cache
+    client.get(cacheKey, async(error, result) => {
+      if (result == null)
+      {
+        console.log("Cache miss")
+        generatePNG(req, res, client)
+      }
+      else
+      {
+        console.log("Cache hit")
+        res.set('Content-Type', 'image/png');
+        res.send(result);
+      }
+    })
+  })
+})
+
+// Functions
+
+async function generatePNG(req, res, redis) {
+  // Params
+  const style = req.params.style
+  const currency = req.params.currency
+  const size = req.params.size
+  const cacheKey = req.path
+    
   // SVG file path
   const svgPath = path.join(__dirname, 'public', 'svg', style, currency + '.svg');
-  
+
   // Check if file exists
   if (!fs.existsSync(svgPath)) 
   {
     res.status(404).send(null);
     return
   }
-  
+
   const svg = fs.readFileSync(svgPath, 'utf8');  
   const element = document.createElement('div')
   element.innerHTML = svg
-  
+
   const svgElement = element.getElementsByTagName("svg")[0]
-  
+
   // Set viewBox so SVG resizes correctly
   const originalSize = svgElement.getAttribute('width')
   svgElement.setAttribute('viewBox', '0 0 ' + originalSize + ' ' + originalSize)
-  
+
   // Set requested size
   svgElement.setAttribute('width', size)
   svgElement.setAttribute('height', size)
-  
+
   // Convert to PNG
   const png = await convert(element.innerHTML, {
     'height' : size, 
     'width' : size,
     'puppeteer' : {'args' : ['--no-sandbox', '--disable-setuid-sandbox']}
   });
+
+  // Save to redis
+  if (redis != null)
+  {
+    redis.set(cacheKey, png);
+  }
   
+  // Return response
   res.set('Content-Type', 'image/png');
   res.send(png);
-})
+} 
 
 // Listen
 var port = process.env.PORT || 3000;
